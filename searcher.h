71fd1138f152f2038e73f2065e1bcaa84298db12
Administrator <develop@ubuntu.(none)>
2012-09-30 00:00:33 +0800

    init

---
+++ searcher.h
@@ -0,0 +1,34 @@
+#define DATA_ELEMENT_LEN 64
+
+typedef struct {
+    unsigned char c;
+    unsigned int friend;
+    unsigned int next;
+    int data;
+} node;
+
+typedef struct {
+    unsigned char d[DATA_ELEMENT_LEN];
+} data;
+
+typedef struct {
+    unsigned char *head;
+    node *body;
+    void *data;
+} content;
+
+#define HAS_NO_ORDER 1
+#define HAS_ORDER_BUT_NEEDNT_CAT 2
+#define HAS_ORDER 3
+
+#define DEBUG 0
+
+int init(void **c, long len) ;
+int release(void *c) ;
+void* parseRuleFile(unsigned char* fileName, int *nn) ;
+int parseRuleLine(void *c, unsigned char* line, int len, int *np);
+int search(void *c, unsigned char *s, int len, int level);
+int searchOne(void *c, unsigned char *s, int pos, int len, int *m);
+int bestResult(unsigned char *s, int pos, int len, node *head, node *n, int * best_pos, int *best_level);
+int saveToFile(void *c, int nn, unsigned char *fileName) ;
+long releaseFromFile(unsigned char *fileName, void **c);
